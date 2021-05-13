5338c6f0742a383bd4ffe1cd53187b5a1661990c
Srinivas Mahindrakar <srinivas.mahindrakar@houzeo.com>
2021-05-14 04:03:17 +0530

    first commit Houzeo Assignment

---
+++ app/Http/Controllers/Property_details.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Property_details extends Controller
+{
+    //
+}
---
+++ database/seeds/dumpDataInPropertyImages.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Property_images;
+class dumpDataInPropertyImages extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $details = DB::table('property_location')->get();
+        $images = false;
+        foreach($details as $key => $detail) {
+            
+            $name   = time().'.jpg';
+            $images[] = [
+                'house_id'      => $detail->house_id,
+                'user_id'       => $detail->user_id,
+                'img_name'      => $name,
+                'primary_image' => $name,
+                'image_caption' => 'Image Caption '. $key,
+                'created_at'    => $detail->created_at,
+                'updated_at'    => $detail->created_at
+             ];
+        }
+        
+        // dump data in property_images table using insert batch
+        
+    }
+}
