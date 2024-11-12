b106b01d3f0210d9909df32e35c08ef1fceadd70
swapnil-borhade74 <swapnil.borhade74@gmail.com>
2024-11-12 09:20:42 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Property_details;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/property-details', 'Property_details@index'); 
-Route::get('/form-validation', 'Property_details@form_validation'); 
+// Route::get('/property-details', 'Property_details@index'); 
+// Route::get('/form-validation', 'Property_details@form_validation'); 
+
+
+Route::get('/property-details', [Property_details::class, 'index']); 
+Route::get('/form-validation', [Property_details::class, 'form_validation']); 
